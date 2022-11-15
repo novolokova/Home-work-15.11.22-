fef00a4328e55b40e5347232c5210435eb013ce8
Margarita <m.novolokova@gmail.com>
2022-11-15 20:03:01 +0200

    Task 2

--- index.js
+++ index.js
@@ -2,13 +2,10 @@
 // доки він його не введе функція брудна, приймає початок і кінець діапазону,
 // а повертає виключно коректне число числового типу!!!!!!
 
-const MIN_NUM = 15;
-const MAX_NUM = 35;
-
 /**
- * 
- * @param {string} MIN_NUM 
- * @param {string} MAX_NUM 
+ *
+ * @param {string} MIN_NUM
+ * @param {string} MAX_NUM
  * @returns {number}
  */
 const guessNumber = function (MIN_NUM = 15, MAX_NUM = 35) {
@@ -23,4 +20,30 @@ const guessNumber = function (MIN_NUM = 15, MAX_NUM = 35) {
   }
 };
 
-guessNumber(2, 55);
+//guessNumber(2, 55);
+
+// Написати скрипт, який виводить числа (числа повинні бути парними і кратними 9)
+// з діапазону від 0 до 100 у зворотньому порядку.(* - це повинна бути функція)
+
+/**
+ *
+ * @param {number} text
+ */
+const showOutput = function (value) {
+  console.log(value);
+};
+
+/**
+ *
+ * @param {number} START_RANGE
+ * @param {number} END_RANGE
+ */
+const findNumber = function (START_RANGE = 0, END_RANGE = 100) {
+  for (let i = END_RANGE; i > START_RANGE; i -= 2) {
+    if (i % 9 != 0) {
+      continue;
+    }
+    showOutput(i);
+  }
+};
+findNumber();
